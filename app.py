5429ad28bb6295835eb82ad503e21c8002b7de9a
Gjonni <giovanni.filice@icloud.com>
2022-10-10 15:13:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ import _thread
 from concurrent.futures import ThreadPoolExecutor
 
 # LOGGING
-logging.basicConfig(format='%(asctime)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p', level=os.environ.get("LOGLEVEL", "DEBUG"))
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    level=os.environ.get("LOGLEVEL", "DEBUG"),
+)
 logger = logging.getLogger("route.response.time")
 
 ### Openshift or Kubernetes
@@ -20,14 +23,13 @@ urllib3.disable_warnings()
 
 if "OPENSHIFT_BUILD_NAME" in os.environ:
     kubernetes.config.load_incluster_config()
-    file_namespace = open(
-        "/run/secrets/kubernetes.io/serviceaccount/namespace", "r")
+    file_namespace = open("/run/secrets/kubernetes.io/serviceaccount/namespace", "r")
     if file_namespace.mode == "r":
         namespace = file_namespace.read()
         print(f"namespace: { namespace }")
 else:
     kubernetes.config.load_kube_config()
-    namespace = 'passbolt'
+    namespace = "passbolt"
 
 
 k8s_client = kubernetes.client.ApiClient()
@@ -38,32 +40,35 @@ def validation_resources():
     if "REQUEST_MEMORY" not in os.environ:
         logger.debug(f"Failed because REQUEST_MEMORY  is not set.")
         raise EnvironmentError(f"Failed because REQUEST_MEMORY is not set.")
-    return {'requests': {'memory': list((os.environ.get("REQUEST_MEMORY"),)), 'cpu': []}, 'limits': {'memory': ['512Mi', '2Gi'], 'cpu': []}}
+    return {
+        "requests": {"memory": list((os.environ.get("REQUEST_MEMORY"),)), "cpu": []},
+        "limits": {"memory": ["512Mi", "2Gi"], "cpu": []},
+    }
 
 
 def validation_namespace():
     if "NAMESPACES" not in os.environ:
         logger.debug("Failed because NAMESPACES is not set.")
         raise EnvironmentError("Failed because NAMESPACES is not set.")
-    return list((os.environ.get("NAMESPACES", 'passbolt'),))
+    return list((os.environ.get("NAMESPACES", "passbolt"),))
 
 
 def validation_exclude():
     if "EXCLUDE" not in os.environ:
         logger.debug("Failed because EXCLUDE is not set.")
         raise EnvironmentError("Failed because NAMESPACES is not set.")
-    return list((os.environ.get("EXCLUDE", 'test-d'),))
+    return list((os.environ.get("EXCLUDE", "test-d"),))
 
 
 def scale_down(kind, name, namespace):
     resources = dyn_client.resources.get(api_version="v1", kind=kind)
     body = {
-        'kind': kind,
-        'apiVersion': 'v1',
-        'metadata': {'name': name},
-        'spec': {
-            'replicas': 0,
-        }
+        "kind": kind,
+        "apiVersion": "v1",
+        "metadata": {"name": name},
+        "spec": {
+            "replicas": 0,
+        },
     }
     resources.patch(body=body, namespace=namespace)
 
@@ -71,23 +76,43 @@ def scale_down(kind, name, namespace):
 def ocp(kind):
     v1_ocp = dyn_client.resources.get(api_version="v1", kind=kind)
     for object in v1_ocp.watch(namespace=namespace):
-        if not (object['object'].metadata.namespace in validation_namespace() and object['object'].metadata.name not in validation_exclude()):
+        if not (
+            object["object"].metadata.namespace in validation_namespace()
+            and object["object"].metadata.name not in validation_exclude()
+        ):
             continue
-        if not object['type'] != "ADDED" or object['type'] != "MODIFIED":
-            continue
-        for container in object['object'].spec.template.spec.containers:
-            if container.resources:
-                if container.resources.requests and container.resources.requests.memory and container.resources.requests.memory not in validation_resources()['requests']['memory']:
-                    logger.debug(
-                        f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.requests.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
-                    scale_down(
-                        object['object'].kind, object['object'].metadata.name, object['object'].metadata.namespace)
+        if object["type"] == "ADDED" or object["type"] == "MODIFIED":
+            for container in object["object"].spec.template.spec.containers:
+                if container.resources:
+                    if (
+                        container.resources.requests
+                        and container.resources.requests.memory
+                        and container.resources.requests.memory
+                        not in validation_resources()["requests"]["memory"]
+                    ):
+                        logger.debug(
+                            f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.requests.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 "
+                        )
+                        scale_down(
+                            object["object"].kind,
+                            object["object"].metadata.name,
+                            object["object"].metadata.namespace,
+                        )
 
-                if container.resources.limits and container.resources.limits.memory and container.resources.limits.memory not in validation_resources()['limits']['memory']:
-                    logger.debug(
-                        f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.limits.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
-                    scale_down(
-                        object['object'].kind, object['object'].metadata.name, object['object'].metadata.namespace)
+                    if (
+                        container.resources.limits
+                        and container.resources.limits.memory
+                        and container.resources.limits.memory
+                        not in validation_resources()["limits"]["memory"]
+                    ):
+                        logger.debug(
+                            f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.limits.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 "
+                        )
+                        scale_down(
+                            object["object"].kind,
+                            object["object"].metadata.name,
+                            object["object"].metadata.namespace,
+                        )
 
 
 def main():
