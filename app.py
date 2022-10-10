38067a6b3ec9c81d0e8666acc8c2915ff189f0ac
Gjonni <giovanni.filice@icloud.com>
2022-10-10 17:23:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def ocp(kind):
         for container in object["object"].spec.template.spec.containers:
             if not container.resources:
                 continue
-            print(container.resources)
+            print(container.resources.requests.memory)
             if  container.resources.requests and container.resources.requests.memory and (container.resources.requests.memory not in validation_resources()["requests"]["memory"]):
                 logger.debug(f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.requests.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
                 scale_down(object["object"].kind,object["object"].metadata.name,object["object"].metadata.namespace,)
