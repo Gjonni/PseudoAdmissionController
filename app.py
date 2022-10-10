d2b4724f4ccda840243070b9f7afcef59b93f41f
Gjonni <giovanni.filice@icloud.com>
2022-10-10 16:14:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,15 +87,14 @@ def ocp(kind):
         for container in object["object"].spec.template.spec.containers:
             print(container.resources.requests)
             print(container.resources.requests.memory)
-            if container.resources:       
-                if  container.resources.requests and container.resources.requests.memory and (container.resources.requests.memory not in validation_resources()["requests"]["memory"]):
-                    logger.debug(f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.requests.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
-                    scale_down(object["object"].kind,object["object"].metadata.name,object["object"].metadata.namespace,)
+            if  container.resources.requests.memory not in validation_resources()["requests"]["memory"]:
+                logger.debug(f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.requests.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
+                scale_down(object["object"].kind,object["object"].metadata.name,object["object"].metadata.namespace,)
 
-                if (container.resources.limits and container.resources.limits.memory and ( container.resources.limits.memory not in validation_resources()["limits"]["memory"]  )  ):
-                    logger.debug(f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.limits.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
-                    scale_down(object["object"].kind, object["object"].metadata.name, object["object"].metadata.namespace,
-                    )
+            if  container.resources.limits.memory not in validation_resources()["limits"]["memory"]:
+                logger.debug(f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.limits.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
+                scale_down(object["object"].kind, object["object"].metadata.name, object["object"].metadata.namespace,
+                )
 
 
 def main():
