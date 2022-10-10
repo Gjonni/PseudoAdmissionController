70fb6f5e44312c40d930c5df21048890f2686962
Gjonni <giovanni.filice@icloud.com>
2022-10-10 14:43:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 import logging
 import sys
 import _thread
+from concurrent.futures import ThreadPoolExecutor
 
 ##### LOGGING
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=os.environ.get("LOGLEVEL", "DEBUG"))
@@ -32,20 +33,23 @@ dyn_client = DynamicClient(k8s_client)
 
 
 def validation_resources():
-   ## return { 'requests': {'memory': [os.environ.get("MEMORY"]),'cpu': [os.environ.get("CPU")] },'limits': {'memory': [os.environ.get("MEMORY"]),'cpu': [os.environ.get("CPU"]) } }
-    return { 'requests': {'memory': ['10Mi','20Mi','2Gi'],'cpu': [] },'limits': {'memory': ['512Mi','2Gi'],'cpu': [] } }
+
+    if "REQUEST_MEMORY" not in os.environ :
+        logger.debug(f"Failed because REQUEST_MEMORY  is not set.")
+        raise EnvironmentError(f"Failed because REQUEST_MEMORY is not set.")
+    return { 'requests': {'memory': list((os.environ.get("REQUEST_MEMORY"),)),'cpu': [] },'limits': {'memory': ['512Mi','2Gi'],'cpu': [] } }
 
 def validation_namespace():
-    if "NAMESPACES" in os.environ:
-        return [os.environ.get("NAMESPACES",'passbolt')]
-    else:
-        return ['passbolt']
+    if "NAMESPACES" not in os.environ:
+        logger.debug("Failed because NAMESPACES is not set.")
+        raise EnvironmentError("Failed because NAMESPACES is not set.")
+    return list((os.environ.get("NAMESPACES",'passbolt'),))
 
 def validation_exclude():
-    if "EXCLUDE" in os.environ:
-        return [os.environ.get("EXCLUDE",'test-d')]
-    else:
-        return ['test-d']
+    if "EXCLUDE" not in os.environ:
+        logger.debug("Failed because EXCLUDE is not set.")
+        raise EnvironmentError("Failed because NAMESPACES is not set.")
+    return list((os.environ.get("EXCLUDE",'test-d'),))
 
 
 def scale_down(kind,name,namespace):
@@ -61,7 +65,7 @@ def scale_down(kind,name,namespace):
     resources.patch(body=body, namespace=namespace)
 
 
-def ocp(threadName, delay,kind):
+def ocp(kind):
     v1_ocp = dyn_client.resources.get(api_version="v1", kind=kind)
     for object in v1_ocp.watch(namespace=namespace):
         if object['object'].metadata.namespace in validation_namespace() and object['object'].metadata.name not in validation_exclude() :
@@ -69,21 +73,25 @@ def ocp(threadName, delay,kind):
                 for container in object['object'].spec.template.spec.containers:
                         if container.resources:
                             if container.resources.requests and container.resources.requests.memory and container.resources.requests.memory not in validation_resources()['requests']['memory']:
-                                logger.debug(f"{threadName} - Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.requests.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
+                                logger.debug(f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.requests.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
                                 scale_down( object['object'].kind , object['object'].metadata.name, object['object'].metadata.namespace)
                             
                             if container.resources.limits and container.resources.limits.memory and container.resources.limits.memory not in validation_resources()['limits']['memory'] :
-                                logger.debug(f"{threadName} - Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.limits.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
+                                logger.debug(f"Policy Violation from Container { container.name } - nella { kind } { object['object'].metadata.name } - { container.resources.limits.memory } in namespace { object['object'].metadata.namespace } - Scale to 0 ")
                                 scale_down( object['object'].kind , object['object'].metadata.name, object['object'].metadata.namespace)
 
 
 
 def main():
-    _thread.start_new_thread( ocp, ("DeploymentConfig-Thread", 2, "DeploymentConfig" ) )
-    _thread.start_new_thread( ocp, ("Deployment-Thread", 4, "Deployment" ) )
-    
-    while 1:
-        pass
+    with ThreadPoolExecutor(max_workers=4) as e:
+        e.submit(ocp,"DeploymentConfig")
+        e.submit(ocp,"Deployment")
+        e.shutdown(wait=True, cancel_futures=False)
+
+ #   _thread.start_new_thread( ocp, ("DeploymentConfig-Thread", 2, "DeploymentConfig" ) )
+ #   _thread.start_new_thread( ocp, ("Deployment-Thread", 4, "Deployment" ) ) 
+  #  while 1:
+  #      pass
 
 
 if __name__ == "__main__":
