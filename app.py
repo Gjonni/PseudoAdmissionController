0ee7b945a013f2ce96ff2ef2afb7bbb9d156bb4d
Gjonni <giovanni.filice@icloud.com>
2022-10-10 11:23:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ dyn_client = DynamicClient(k8s_client)
 
 def validation_resources():
     if "REQUESTMEMORY" in os.environ:
-        return { 'requests': {'memory': list((os.environ.get("REQUESTMEMORY"))),'cpu': [] },'limits': {'memory': ['512Mi','2Gi'],'cpu': [] } }
+        return { 'requests': {'memory': list((os.environ.get("REQUESTMEMORY"))),'cpu': list((os.environ.get("REQUESTCPU"))) },'limits': {'memory': ['512Mi','2Gi'],'cpu': [] } }
     else:
         logging.debug(f"Proceeding since environment variable REQUESTMEMORY exists ")
    ## return { 'requests': {'memory': [os.environ.get("MEMORY"]),'cpu': [os.environ.get("CPU")] },'limits': {'memory': [os.environ.get("MEMORY"]),'cpu': [os.environ.get("CPU"]) } }
